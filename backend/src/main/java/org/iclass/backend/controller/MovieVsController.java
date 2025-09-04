18025db7571fefe48d90699131f055982d9483fa
IMJEONGSU <3353513a@gmail.com>
2025-09-04 14:48:29 +0900

    rank

--- backend/src/main/java/org/iclass/backend/controller/MovieVsController.java
+++ backend/src/main/java/org/iclass/backend/controller/MovieVsController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/vs")
+@RequestMapping("/api/vs")
 @RequiredArgsConstructor
 public class MovieVsController {
 
