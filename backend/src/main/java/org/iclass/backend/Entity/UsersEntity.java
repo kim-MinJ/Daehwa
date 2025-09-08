d986fc39b6f3af51ece96404fefd7b515b817e7a
suyoung0211 <suyoung332211@gmail.com>
2025-09-08 10:07:40 +0900

    Revert "Server"

--- backend/src/main/java/org/iclass/backend/Entity/UsersEntity.java
+++ backend/src/main/java/org/iclass/backend/Entity/UsersEntity.java
@@ -7,7 +7,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,8 +19,7 @@ import lombok.ToString;
 @ToString
 @Table(name = "Users")
 @Entity
-@Builder
-public class UsersEntity {  
+public class UsersEntity {
   @Id
   @Column(name = "user_id", length = 100)
   private String userId;
@@ -33,14 +31,11 @@ public class UsersEntity {
   private String password;
 
   @Column(length = 20)
-  @Builder.Default
   private String role = "user";
 
   @Column(name = "reg_date")
-  @Builder.Default
   private LocalDateTime regDate = LocalDateTime.now();
 
   @Column
-  @Builder.Default
-  private Integer status = 0; // 로그인 상태 1:로그인 0:로그아웃
+  private Integer status = 0; // 로그인 상태 0:로그인 1:로그아웃
 }
